3360b20ae93ba92f48de336ed2c9dbecbc007886
Ray <ray.gabriel@predify.me>
2024-01-23 22:30:42 +0000

    Add failing tests for Buzz rule using NUnit.

---
+++ FizzBuzzApp/BuzzRuleTests.cs
@@ -0,0 +1,38 @@
+﻿using FizzBuzz.Rules;
+
+namespace FizzBuzzApp.Tests
+{
+    [TestFixture]
+    public class BuzzRuleTests
+    {
+        private BuzzRule _buzzRule;
+
+        [SetUp]
+        public void Setup()
+        {
+            _buzzRule = new BuzzRule();
+        }
+
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(20)]
+        public void BuzzRule_ShouldApplyToBuzzNumbers(int number)
+        {
+            Assert.IsTrue(_buzzRule.IsApplicable(number));
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void BuzzRule_ShouldNotApplyToNonBuzzNumbers(int number)
+        {
+            Assert.IsFalse(_buzzRule.IsApplicable(number));
+        }
+
+        [Test]
+        public void BuzzRule_ShouldReturnBuzz()
+        {
+            Assert.AreEqual("Buzz", _buzzRule.GetResult());
+        }
+    }
+}
